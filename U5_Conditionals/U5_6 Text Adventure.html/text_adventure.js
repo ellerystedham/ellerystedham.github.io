f8ae47fea07525d2a83a4c40b99b987af4a55fae
ellerystedham <115045387+ellerystedham@users.noreply.github.com>
2022-11-10 19:20:58 -0600

    Create text_adventure.js

---
+++ U5_Conditionals/U5_6 Text Adventure.html/text_adventure.js
@@ -0,0 +1,18 @@
+alert("It's your mom's birthday and you decide to bake her a cake.")
+let grocerystore_pantry= prompt("Do you (1) go to the grocery store to buy the ingredients or (2) hope you have all the ingredients in your pantry?");
+if(grocerystore_pantry == 2) {
+    let cold_roomtemperature = prompt("You know that you don't have much time to bake your mom's cake. The recipe calls for room temperature butter and eggs. Do you (1) hope for the best and bake with cold butter and eggs or (2) decide to follow the recipe and wait for the butter and eggs to come to room temp?")
+      if (cold_roomtemperature == 1) {
+        alert("You should have followed the recipe! The batter turns out lumpy and you have to start over!");
+      } else {
+        let chocolate_vanilla = prompt ("The cake has just come out of the oven and looks perfect. It's now time to frost it! Do you choose (1) the chocolate or (2) vanilla frosting for your cake?")
+        if (chocolate_vanilla == 1) {
+            alert("Yay! This is your mom's favorite flavor of icing. You made her the best birthday cake!")
+        } else 
+            if (chocolate_vanilla == 2) {
+                alert ( "The cake is still delicious, but it would have been better with the chocolate frosting. Maybe next time you can make your mom the best cake! ") } {"The cake is still delicious, but it would have been better with the chocolate frosting. Maybe next time you can make your mom the best cake!"
+         }
+      } }
+if (grocerystore_pantry == 1){
+    alert("You get stuck in horrible traffic and don't have time to bake your cake.")} 
+    
